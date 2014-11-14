d5048d0b5d15e353d2449e9946c1bd13d6e71630
Matthieu Schneider <matthieu@yellowcake.net>
2014-11-14 11:58:20 +0100

    Mise en place de la structure définitive

--- app/app.js
+++ app/app.js
@@ -8,9 +8,28 @@ angular.module('app', [
   'app.home',
   'app.view2'
 ]).
+run(function($rootScope, $location, $anchorScroll, $routeParams, snapRemote){
+  $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+    $location.hash($routeParams.scrollTo);
+    $anchorScroll();
+  });
+  snapRemote.getSnapper().then(function(snapper) {
+    snapper.on('close', function(){
+      angular.element(document.querySelector('#snap-toggle')).removeClass('active');
+    });
+    snapper.on('open', function(){
+      angular.element(document.querySelector('#snap-toggle')).addClass('active');
+    });
+  });
+}).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.otherwise({redirectTo: '/home'});
 }]).
 config(function(snapRemoteProvider) {
-  snapRemoteProvider.globalOptions.disable = 'right';
+  snapRemoteProvider.globalOptions = {
+    touchToDrag: false,
+    disable : 'right',
+    maxPosition: 265,
+    minPosition: -265
+  };
 });
--- app/home/home.js
+++ app/home/home.js
@@ -9,6 +9,91 @@ angular.module('app.home', ['ngRoute'])
   });
 }])
 
-.controller('HomeCtrl', [function() {
+.controller('HomeCtrl', ['$scope', function($scope) {
+  $scope.settings = {
+    author: {
+      name: 'Matthieu Schneider',
+      job: 'Senior Front-End Web Developer',
+      picture: 'files/matthieu-schneider_front-end-web-developer.jpg',
+      resume: 'files/matthieu-schneider_front-end-web-developer.pdf',
+      headline: 'Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      description: 'Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque.',
+      birthday: '27/02/1998',
+      phone: '+33 6 45 79 09 99',
+      email: 'matthieu.snd@gmail.com',
+      address: 'Rennes - FRANCE'
+    },
+    profiles : [
+      {
+        link: 'https://twitter.com/Matthieusch',
+        service: 'twitter'
+      },
+      {
+        link: 'fr.linkedin.com/in/matthieusch/',
+        service: 'linkedin'
+      }
+    ],
+    hobbies : [
+      {
+        id: 'cycle',
+        name: 'Pignon fixe'
+      },
+      {
+        id: 'coffe',
+        name: 'Café'
+      },
+      {
+        id: 'gamepad',
+        name: 'Jeux vidéos'
+      },
+      {
+        id: 'facetime-video',
+        name: 'Cinéma'
+      },
+      {
+        id: 'headphones',
+        name: 'Musique'
+      },
+      {
+        id: 'apple',
+        name: 'Mac OS'
+      },
+      {
+        id: 'rocket',
+        name: 'Astronomie'
+      },
+      {
+        id: 'beer',
+        name: 'Bière'
+      }
+    ]
+  };
+
+  $scope.settings.hobbies = shuffle($scope.settings.hobbies);
+
+  $scope.backgrounds = [
+    {src: 'files/backgrounds/background-1.jpg'},
+    {src: 'files/backgrounds/background-2.jpg'}
+  ];
+  $scope.background = $scope.backgrounds[Math.floor((Math.random() * 2))];
+
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex ;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
 
 }]);
